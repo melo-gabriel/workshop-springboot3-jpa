0f41e904a8ce939421f783cda058474d4487f226
Gabriel Silva <silva.gabrielmelosantos@gmail.com>
2023-04-16 16:35:31 -0300

    Service layer, component registration

---
+++ src/main/java/com/gabriel/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.gabriel.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.course.entities.User;
+import com.gabriel.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+		
+	
+
+}
