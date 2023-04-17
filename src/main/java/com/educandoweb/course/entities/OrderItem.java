25ae63276c7eece5865c7fe13803198f7645e98c
Gabriel Silva <silva.gabrielmelosantos@gmail.com>
2023-04-17 12:01:13 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,93 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
+	
+	private Integer quantiy;
+	private Double price;
+	
+	public OrderItem(){	
+	}
+
+	public OrderItem(Order order, Product product, Integer quantiy, Double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantiy = quantiy;
+		this.price = price;
+	}
+
+	@JsonIgnore
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+
+	public Integer getQuantiy() {
+		return quantiy;
+	}
+
+	public void setQuantiy(Integer quantiy) {
+		this.quantiy = quantiy;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
